5de38604e51bd1607d32140aeb9697ac932ca2d7
GersonESS <gebhsantos@gmail.com>
2025-12-09 22:10:50 -0300

    a

---
+++ 02/prisma.config.js
@@ -0,0 +1,20 @@
+// Importa e configura o dotenv para carregar variáveis de ambiente
+require("dotenv/config");
+
+// Importa as funções necessárias do pacote de configuração do Prisma// Importa e configura o dotenv para carregar variáveis de ambiente
+require("dotenv/config");
+
+// Importa as funções necessárias do pacote de configuração do Prisma
+const { defineConfig, env } = require("prisma/config");
+
+// Exporta a configuração
+module.exports = defineConfig({
+  // Caminho para o seu schema do Prisma
+  schema: "prisma/schema.prisma",
+  
+  // Define a fonte de dados
+  datasource: {
+    // Pega a URL do banco de dados da variável de ambiente DATABASE_URL
+    url: env("DATABASE_URL"),
+  },
+});

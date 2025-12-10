cceb270412392c3b9ebfc93096f4c5111bf3a692
GersonESS <gebhsantos@gmail.com>
2025-12-09 21:29:02 -0300

    a

--- 02/server.js
+++ 02/server.js
@@ -1,15 +1,35 @@
-import express from 'express'
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient();
 
-app.use(express.json())
+app.use(express.json());
 
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, senha, tel } = req.body;
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        senha,
+        tel,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Servidor rodando na porta ${port}`);
+});
